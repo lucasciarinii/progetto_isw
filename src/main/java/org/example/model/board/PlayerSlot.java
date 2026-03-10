3c49b48b1577cd6e6b6567d25f1e3fdefad8c54d
Luca Sciarini <luca.sciarini04@gmail.com>
2026-03-10 13:58:44 +0100

    feat: class "PlayerSlot"

--- src/main/java/org/example/model/board/PlayerSlot.java
+++ src/main/java/org/example/model/board/PlayerSlot.java
@@ -1,5 +1,17 @@
 package org.example.Model.board;
 
 public class PlayerSlot {
+    private String playerName;
 
+    public PlayerSlot(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
 }
