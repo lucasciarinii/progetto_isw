e41837dfeba88f4f9a850c669c992cb1dd473d7b
Daniel Russo <russo.daniel04@gmail.com>
2026-03-10 14:46:36 +0100

    edit: TurnOrderTile

--- src/main/java/org/example/model/board/TurnOrderTile.java
+++ src/main/java/org/example/model/board/TurnOrderTile.java
@@ -3,7 +3,7 @@ package org.example.model.board;
 import java.util.List;
 
 public class TurnOrderTile {
-    private List<PlayerSlotEffect> slots;
+    private final List<PlayerSlotEffect> slots;
 
     public TurnOrderTile(List<PlayerSlotEffect> slots) {
         this.slots = slots;
