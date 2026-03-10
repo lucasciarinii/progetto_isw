6c11c6713f96492da52302cb5ebdf0b25a4ed507
Daniel Russo <russo.daniel04@gmail.com>
2026-03-10 17:11:36 +0100

    edit: TurnOrderTile

---
+++ src/main/java/org/example/model/match/GameState.java
@@ -0,0 +1,41 @@
+package org.example.model.match;
+
+import java.util.List;
+
+public class GameState {
+    private int currentRound;
+    private GamePhase currentPhase;
+    private List<Player> turnOrder;
+    private int currentPlayerIndex;
+    private Player winner;
+
+    public GameState() {
+    }
+
+    public int getCurrentRound(){return currentRound;}
+
+    public GamePhase getCurrentPhase(){return currentPhase;}
+
+    public Player getCurrentPlayer(){return turnOrder[currentPlayerIndex];}
+
+    public List<Player> getTurnOrder(){return turnOrder;}
+
+    public Player getWinner(){return winner;}
+
+    public boolean isGameOver(){}
+
+    public void advanceToNextPlayer(){
+        if (currentPlayerIndex==turnOrder.lenght()-1) {
+            currentPlayerIndex=0;
+            advancePhase();
+        }
+        else currentPlayerIndex++;
+    }
+
+    public void advancePhase(){}
+
+    public void updateTurnOrder(List<Player> newOrder){turnOrder=newOrder;}
+
+    public void setWinner(Player player){winner=player;}
+
+}
