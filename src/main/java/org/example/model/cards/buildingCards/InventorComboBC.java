e601abc3cbf40641dcc0ae2e1113668370a80f0c
EdoardoSacchi <edosacchi.04@gmail.com>
2026-03-14 15:11:25 +0100

    feat: add InventorComboBC class for building card functionality

---
+++ src/main/java/org/example/model/cards/buildingCards/InventorComboBC.java
@@ -0,0 +1,20 @@
+package org.example.model.cards.buildingCards;
+
+import org.example.model.board.Board;
+import org.example.model.enums.Era;
+import org.example.model.match.Player;
+
+public class InventorComboBC extends BuildingCard{
+
+
+    public InventorComboBC(int id, Era era, int foodCost, int endPoints, boolean isEndGame) {
+        super(id, era, foodCost, endPoints, isEndGame);
+    }
+
+    
+    @Override
+    public void applyEffect(Player player, Board board) {
+        System.out.println("Ciao");
+    }
+    
+}
