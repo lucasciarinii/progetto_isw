867c38c13b5998e939621d878c680f35f51d39c0
leotaccari <leotaccari04@gmail.com>
2026-03-14 16:50:25 +0100

    feat: add CharacterEndPointsBC class and update SustenanceDiscountBC to include character effect

---
+++ src/main/java/org/example/model/cards/buildingCards/CharacterEndPointsBC.java
@@ -0,0 +1,22 @@
+package org.example.model.cards.buildingCards;
+
+import org.example.model.enums.Era;
+import org.example.model.match.Player;
+import org.example.model.enums.CharacterType;
+
+public class CharacterEndPointsBC extends BuildingCard {
+    private final int pointsEffect;
+    private final CharacterType characterEffect;
+
+    public CharacterEndPointsBC(int id, Era era, int foodCost, int endPoints, boolean isEndGame, int points, CharacterType characterEffect) {
+		super(id, era, foodCost, endPoints, isEndGame);
+        this.pointsEffect = points;
+        this.characterEffect = characterEffect;
+	}
+
+	@Override
+	public void applyEffect(Player p) {
+
+	}
+}
+
--- src/main/java/org/example/model/cards/buildingCards/SustenanceDiscountBC.java
+++ src/main/java/org/example/model/cards/buildingCards/SustenanceDiscountBC.java
@@ -2,11 +2,14 @@ package org.example.model.cards.buildingCards;
 
 import org.example.model.enums.Era;
 import org.example.model.match.Player;
+import org.example.model.enums.CharacterType;
 
 public class SustenanceDiscountBC extends BuildingCard {
+	private final CharacterType characterEffect;
 
-	public SustenanceDiscountBC(int id, Era era, int foodCost, int endPoints, boolean isEndGame) {
+	public SustenanceDiscountBC(int id, Era era, int foodCost, int endPoints, boolean isEndGame, CharacterType characterEffect) {
 		super(id, era, foodCost, endPoints, isEndGame);
+		this.characterEffect = characterEffect;
 	}
 
 	@Override
