3a9c3147b653f8d8823a244f9d123941ea900d21
Luca Sciarini <luca.sciarini04@gmail.com>
2026-03-14 10:35:24 +0100

    feat: add JSON serialization support for card subclasses in Card.java

--- src/main/java/org/example/model/cards/Card.java
+++ src/main/java/org/example/model/cards/Card.java
@@ -1,6 +1,26 @@
 package org.example.model.cards;
 
+import org.example.model.cards.buildingCards.EndGameBonusBC;
+import org.example.model.cards.buildingCards.EventBoostBC;
+import org.example.model.cards.buildingCards.RoundFlowBC;
 import org.example.model.enums.Era;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+// Indicates to Jackson to use the "class_type" field to decide the subclass
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "class_type"
+)
+// Map values of the "class_type" field to the actual Java classes
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = EventBoostBC.class, name = "EventBoostBC"),
+        @JsonSubTypes.Type(value = RoundFlowBC.class, name = "RoundFlowBC"),
+        @JsonSubTypes.Type(value = EndGameBonusBC.class, name = "EndGameBonusBC"),
+
+        // TODO: ADD REMAINING CARD TYPES
+})
 
 public abstract class Card {
 
