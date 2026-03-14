d2348faf38646293ee54183953af42f2219a59e0
EdoardoSacchi <edosacchi.04@gmail.com>
2026-03-14 18:43:28 +0100

    feat: rename 'type' to 'shouldDoublePrestigePoints' for clarity and implement applyShamanicRitualEffect logic

--- src/main/java/org/example/model/cards/buildingCards/ShamanicPointsBC.java
+++ src/main/java/org/example/model/cards/buildingCards/ShamanicPointsBC.java
@@ -9,11 +9,11 @@ import java.util.ArrayList;
 
 public class ShamanicPointsBC extends BuildingCard {
 
-    private final boolean type;
+    private final boolean shouldDoublePrestigePoints;
 
     public ShamanicPointsBC(int id, Era era, int foodCost, int endPoints, boolean isEndGame, boolean type) {
         super(id, era, foodCost, endPoints, isEndGame);
-        this.type = type;
+        this.shouldDoublePrestigePoints = type;
     }
 
     @Override
@@ -22,7 +22,54 @@ public class ShamanicPointsBC extends BuildingCard {
     }
 
 
-    private void applyShamanicRitualEffect(Player owner, Board board, ArrayList<Player> players, ShamanicRitual shamanicRitualEvent) {
+    private boolean applyShamanicRitualEffect(Player owner, Board board, ArrayList<Player> players,
+                                           ShamanicRitual shamanicRitualEvent) {
+
+
+        // During the Shamanic Ritual, if it's the first type of card, if the player has more (or the same number)
+        // Shamanic Stars than the other players, he earns double of Prestige Points.
+        if ( !shouldDoublePrestigePoints) {
+
+            for (Player p : players) {
+
+                // skip cycle if the player in the List is the owner
+                if ( p.equals(owner) ) {
+                    continue;
+                }
+
+                // check if the player in the List has more Shamanic Stars than the owner of the building, it returns
+                // false to the controller, and it doesn't apply the effect
+                if ( p.getShamansStars() > owner.getShamansStars() ) {
+                    return false;
+                }
+                
+            }
+
+            return true;
+
+        }
+
+        // During a Shamanic Ritual, if it's the second type of card, if the player has less Shamanic Stars than any
+        // other player, he doesn't lose Prestige Points
+        else {
+
+            for (Player p : players ) {
+
+                // skip cycle if the player in the List is the owner
+                if ( p.equals(owner) ) {
+                    continue;
+                }
+
+                // check if the player in the list has less Shamanic Stars than the owner of the building, it returns
+                // false to the controller, and it doesn't apply the effect
+                if ( p.getShamansStars() < owner.getShamansStars() ) {
+                    return false;
+                }
+
+            }
+
+            return true;
+        }
 
     }
 
