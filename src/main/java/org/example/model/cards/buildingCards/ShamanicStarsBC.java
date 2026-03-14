9de17dd3162bcb65677f26f54322aa077ace307e
EdoardoSacchi <edosacchi.04@gmail.com>
2026-03-15 00:07:48 +0100

    feat: implement applyShamanicRitualEffect method to add shamanic stars points

--- src/main/java/org/example/model/cards/buildingCards/ShamanicStarsBC.java
+++ src/main/java/org/example/model/cards/buildingCards/ShamanicStarsBC.java
@@ -3,9 +3,12 @@ package org.example.model.cards.buildingCards;
 import org.example.model.board.Board;
 import org.example.model.enums.Era;
 import org.example.model.match.Player;
+import org.jetbrains.annotations.NotNull;
 
 public class ShamanicStarsBC extends BuildingCard {
 
+
+    private static final int SHAMANIC_STARS_POINTS = 3;
 
     public ShamanicStarsBC(int id, Era era, int foodCost, int endPoints, boolean isEndGame) {
         super(id, era, foodCost, endPoints, isEndGame);
@@ -14,6 +17,10 @@ public class ShamanicStarsBC extends BuildingCard {
     @Override
     public void applyEffect(Player owner, Board board) {
 
+    }
+
+    public void applyShamanicRitualEffect(Player owner) {
+        owner.addShamanStars(SHAMANIC_STARS_POINTS);
     }
 
 
