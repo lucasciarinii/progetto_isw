385d3cf73018ef351dce13654dd83e58ad024648
leotaccari <leotaccari04@gmail.com>
2026-03-15 17:10:54 +0100

    feat: implement applyEvent method in Sustenance to manage food and points for players based on owned characters

--- src/main/java/org/example/model/cards/eventCards/HuntEvent.java
+++ src/main/java/org/example/model/cards/eventCards/HuntEvent.java
@@ -6,7 +6,6 @@ import org.example.model.match.Context;
 import org.example.model.match.Player;
 import org.example.model.cards.characters.Character;
 import org.example.model.enums.CharacterType;
-
 import java.util.List;
 
 
@@ -20,12 +19,13 @@ public class HuntEvent extends EventCard {
     }
 
     public void applyEvent(Context c) {
+
         //For each player,
         //count the number of hunters they have and give them 1 food
         // and points equal to the number of hunters multiplied by the points value of the card
+
         List<Player> players = c.getPlayers();
-        for (int i = 0; i < players.size(); i++) {
-            Player player = players.get(i);
+        for (Player player : players) {
             int hunters = 0;
 
             for (int j = 0; j < player.getOwnedCharacters().size(); j++) {
--- src/main/java/org/example/model/cards/eventCards/Sustenance.java
+++ src/main/java/org/example/model/cards/eventCards/Sustenance.java
@@ -3,6 +3,8 @@ package org.example.model.cards.eventCards;
 import org.example.model.enums.Era;
 import org.example.model.enums.EventEffect;
 import org.example.model.match.Player;
+import org.example.model.match.Context;
+import java.util.List;
 
 public class Sustenance extends EventCard {
 
@@ -13,26 +15,30 @@ public class Sustenance extends EventCard {
         this.points = points;
     }
 
-    public int getPoints(Player player) {
-        int numCharachters = player.getOwnedCharacters().size();
+    public void applyEvent(Context c) {
 
-        int availableFood = player.getFood();
-        int paidFood;
+        //For each player use 1 food to feed each one of his characters.
+        // If a player cannot feed all his characters,
+        // he loses points equal to the number of unfed characters multiplied by the points value of the card.
 
-        //Check if I can feed all characters
-        if (availableFood >= numCharachters) {
-            paidFood = numCharachters;
-        } else {
-            paidFood = availableFood;
+        List<Player> players = c.getPlayers();
+        for (Player player : players) {
+            int numCharacters = player.getOwnedCharacters().size();
+            int availableFood = player.getFood();
+            int paidFood;
+
+            if (availableFood >= numCharacters) {
+                paidFood = numCharacters;
+            } else {
+                paidFood = availableFood;
+            }
+
+            player.addFood(-paidFood);
+
+            int notFedCharacters = numCharacters - paidFood;
+            int lostPoints = notFedCharacters * points;
+
+            player.addPoints(-lostPoints);
         }
-
-        //Remove food used
-        player.addFood(-paidFood);
-
-        //Calculate point lost depending on notFedCharacters number
-        int notFedCharacters = numCharachters - paidFood;
-        int lostPoints = notFedCharacters * points;
-
-        return lostPoints;
     }
 }
