e3228b110d276df09d94370cb23bfc7752e49a36
leotaccari <leotaccari04@gmail.com>
2026-03-15 17:01:55 +0100

    feat: implement applyEvent method in HuntEvent to calculate food and points for players based on owned hunters

--- src/main/java/org/example/model/cards/eventCards/HuntEvent.java
+++ src/main/java/org/example/model/cards/eventCards/HuntEvent.java
@@ -2,9 +2,12 @@ package org.example.model.cards.eventCards;
 
 import org.example.model.enums.Era;
 import org.example.model.enums.EventEffect;
+import org.example.model.match.Context;
 import org.example.model.match.Player;
 import org.example.model.cards.characters.Character;
 import org.example.model.enums.CharacterType;
+
+import java.util.List;
 
 
 public class HuntEvent extends EventCard {
@@ -16,22 +19,26 @@ public class HuntEvent extends EventCard {
         this.points = points;
     }
 
-    public int getPoints(Player player) {
+    public void applyEvent(Context c) {
+        //For each player,
+        //count the number of hunters they have and give them 1 food
+        // and points equal to the number of hunters multiplied by the points value of the card
+        List<Player> players = c.getPlayers();
+        for (int i = 0; i < players.size(); i++) {
+            Player player = players.get(i);
+            int hunters = 0;
 
+            for (int j = 0; j < player.getOwnedCharacters().size(); j++) {
+                Character character = player.getOwnedCharacters().get(j);
 
-        int hunters = 0;
+                if (character.getCharacterType() == CharacterType.HUNTER) {
+                    hunters++;
+                }
+            }
 
-        for (int i = 0;  i < player.getOwnedCharacters().size(); i++) {
-                Character c = player.getOwnedCharacters().get(i);
-                if(c.getCharacterType() == CharacterType.HUNTER) {hunters++;}
-
-
+            int gainedPoints = hunters * points;
+            player.addFood(1);
+            player.addPoints(gainedPoints);
         }
-        int gainedPoints = hunters * points;
-        player.addFood(1);
-
-        return gainedPoints;
     }
-
-
 }
