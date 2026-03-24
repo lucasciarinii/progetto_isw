48227b63f3b14728c270d574b980abb0aca14e54
EdoardoSacchi <edosacchi.04@gmail.com>
2026-03-24 15:48:56 +0100

    refactor: update PlayerSlot to use Player object and add turn order effect logic

--- src/main/java/org/example/model/board/PlayerSlot.java
+++ src/main/java/org/example/model/board/PlayerSlot.java
@@ -1,19 +1,34 @@
 package org.example.model.board;
 
+import org.example.model.match.Player;
+
 import java.util.Objects;
 
 public class PlayerSlot {
     private String playerName;
+    private final int food;
+    private final int points;
 
-    public PlayerSlot(String playerName) {
-        this.playerName = Objects.requireNonNull(playerName, "Player name cannot be null");
+    public PlayerSlot(Player player, int food, int points) {
+        this.playerName = Objects.requireNonNull(player.getNickname(), "Player name cannot be null");
+        this.food = food;
+        this.points = points;
     }
 
     public String getPlayerName() {
         return playerName;
     }
 
-    public void setPlayerName(String playerName) {
-        this.playerName = playerName;
+
+    public void applyTurnOrderEffect(Player player, int food, int points) {
+        this.playerName = player.getNickname();
+
+        if ( food < 0 && player.getFood() < 1 ) {
+            player.addPoints(points);
+            return;
+        }
+
+        player.addFood(food);
+
     }
 }
