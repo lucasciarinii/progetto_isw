43951116a13edeee206e42fc47209e5337cbec77
EdoardoSacchi <edosacchi.04@gmail.com>
2026-03-24 15:55:07 +0100

    refactor: update PlayerSlot to use Player object and simplify player name retrieval

--- src/main/java/org/example/model/board/PlayerSlot.java
+++ src/main/java/org/example/model/board/PlayerSlot.java
@@ -5,23 +5,23 @@ import org.example.model.match.Player;
 import java.util.Objects;
 
 public class PlayerSlot {
-    private String playerName;
+    private Player player;
     private final int food;
     private final int points;
 
     public PlayerSlot(Player player, int food, int points) {
-        this.playerName = Objects.requireNonNull(player.getNickname(), "Player name cannot be null");
+        this.player = player;
         this.food = food;
         this.points = points;
     }
 
     public String getPlayerName() {
-        return playerName;
+        return player.getNickname();
     }
 
 
     public void applyTurnOrderEffect(Player player, int food, int points) {
-        this.playerName = player.getNickname();
+        this.player = player;
 
         if ( food < 0 && player.getFood() < 1 ) {
             player.addPoints(points);
