5724ee23d14d41745725848cfb2af692156fe273
Luca Sciarini <luca.sciarini04@gmail.com>
2026-03-27 11:57:21 +0100

    refactor: enhance Board and Match classes with - newEraOperations() - endRoundOperations() - resolveBottomEvents() and resolveTopEvents() - TODOs

--- src/main/java/org/example/model/board/Board.java
+++ src/main/java/org/example/model/board/Board.java
@@ -1,10 +1,17 @@
 package org.example.model.board;
 
 import org.example.model.cards.Card;
+import org.example.model.cards.buildingCards.BuildingCard;
+import org.example.model.cards.characters.Character;
+import org.example.model.cards.characters.Inventor;
 import org.example.model.cards.eventCards.EventCard;
+import org.example.model.cards.eventCards.Sustenance;
 import org.example.model.decks.BuildingDeck;
 import org.example.model.decks.MainDeck;
+import org.example.model.enums.Era;
 import org.example.model.enums.OfferEffect;
+import org.example.model.interfaces.Visitor;
+import org.example.model.match.Match;
 import org.example.model.match.Player;
 
 import java.util.ArrayList;
@@ -27,10 +34,14 @@ public class Board {
 
         // offerTrack initialization
         switch (players.size()) {
-            case 2 -> offerTrack = List.of(new OfferTile(OfferEffect.D), new OfferTile(OfferEffect.U), new OfferTile(OfferEffect.DU), new OfferTile(OfferEffect.UU));
-            case 3 -> offerTrack = List.of(new OfferTile(OfferEffect.D), new OfferTile(OfferEffect.U), new OfferTile(OfferEffect.DD) ,new OfferTile(OfferEffect.DU), new OfferTile(OfferEffect.UU));
-            case 4 -> offerTrack = List.of(new OfferTile(OfferEffect.D), new OfferTile(OfferEffect.U), new OfferTile(OfferEffect.DD) ,new OfferTile(OfferEffect.DU), new OfferTile(OfferEffect.UU), new OfferTile(OfferEffect.DUU));
-            case 5 -> offerTrack = List.of(new OfferTile(OfferEffect.FOOD), new OfferTile(OfferEffect.D), new OfferTile(OfferEffect.U), new OfferTile(OfferEffect.DD) ,new OfferTile(OfferEffect.DU), new OfferTile(OfferEffect.UU), new OfferTile(OfferEffect.DUU));
+            case 2 ->
+                    offerTrack = List.of(new OfferTile(OfferEffect.D), new OfferTile(OfferEffect.U), new OfferTile(OfferEffect.DU), new OfferTile(OfferEffect.UU));
+            case 3 ->
+                    offerTrack = List.of(new OfferTile(OfferEffect.D), new OfferTile(OfferEffect.U), new OfferTile(OfferEffect.DD), new OfferTile(OfferEffect.DU), new OfferTile(OfferEffect.UU));
+            case 4 ->
+                    offerTrack = List.of(new OfferTile(OfferEffect.D), new OfferTile(OfferEffect.U), new OfferTile(OfferEffect.DD), new OfferTile(OfferEffect.DU), new OfferTile(OfferEffect.UU), new OfferTile(OfferEffect.DUU));
+            case 5 ->
+                    offerTrack = List.of(new OfferTile(OfferEffect.FOOD), new OfferTile(OfferEffect.D), new OfferTile(OfferEffect.U), new OfferTile(OfferEffect.DD), new OfferTile(OfferEffect.DU), new OfferTile(OfferEffect.UU), new OfferTile(OfferEffect.DUU));
             default -> throw new IllegalArgumentException("Invalid list of players");
         }
 
@@ -38,25 +49,24 @@ public class Board {
         mainDeck = new MainDeck(players.size());
 
         // buildingDeck initialization, based on the number of players (delegated to BuildingDeck constructor)
-        buildingDeck = new BuildingDeck(players.size());
+        buildingDeck = new BuildingDeck(players.size(), this);
 
         // bottomRow and topRow instantiation and initialization
         topRow = new ArrayList<>();
         bottomRow = new ArrayList<>();
 
         // bottomRow initialization: draw cards until we have players.size() + 1 cards in the bottom row, if we draw an event card, put it in the top row (and continue to draw until we have enough cards in the bottom row)
-        for(int i=0; i<players.size() + 1; i++) {
+        for (int i = 0; i < players.size() + 1; i++) {
             Card drawnCard = mainDeck.draw();
-            if(drawnCard instanceof EventCard) {
+            if (drawnCard instanceof EventCard) {
                 topRow.add(drawnCard);
                 i--; // decrement i to draw another card for the bottom row
-            }
-            else
+            } else
                 bottomRow.add(drawnCard);
         }
 
         // topRow initialization: draw cards until we have players.size() + 4 cards in the top row (taking into account the event cards we might have drawn in the previous step)
-        for(int i=0; i<players.size() + 4 - topRow.size(); i++)
+        for (int i = 0; i < players.size() + 4 - topRow.size(); i++)
             topRow.add(mainDeck.draw());
 
     }
@@ -87,3 +97,4 @@ public class Board {
 
 
 }
+
