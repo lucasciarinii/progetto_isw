f7fdb97c7a89094ab945029bd2493eba21b23a79
EdoardoSacchi <edosacchi.04@gmail.com>
2026-05-10 12:32:51 +0200

    feat(network): enhance RMIClientNetworkAdapter with server connection and game actions

--- src/main/java/org/example/network/rmi/RMIClientNetworkAdapter.java
+++ src/main/java/org/example/network/rmi/RMIClientNetworkAdapter.java
@@ -1,11 +1,18 @@
 package org.example.network.rmi;
 
 import org.example.client.ClientController;
+import org.example.client.rmi.RMIClientCallbackImpl;
 import org.example.network.ClientNetworkAdapter;
+import org.example.server.rmi.RMIGameServer;
 
+import java.rmi.Naming;
+
+// Client implementation using RMI
 public class RMIClientNetworkAdapter implements ClientNetworkAdapter {
 
     private final ClientController controller;
+    private RMIGameServer server;
+    private String nickname;
 
     public RMIClientNetworkAdapter(ClientController controller) {
         this.controller = controller;
@@ -13,21 +20,26 @@ public class RMIClientNetworkAdapter implements ClientNetworkAdapter {
 
     @Override
     public void connect(String host, int port, String nickname, int numPlayers) throws Exception {
+        System.setProperty("java.rmi.server.hostname", "localhost");
+        this.nickname = nickname;
 
+        server = (RMIGameServer) Naming.lookup("rmi://" + host + "/GameServer");
+        RMIClientCallbackImpl callback = new RMIClientCallbackImpl(controller);
+        server.register(nickname, port, callback);
     }
 
     @Override
     public void placeTotemOnOfferTile(int tilePosition) throws Exception {
-
+        server.placeTotemOnOfferTile(nickname, tilePosition);
     }
 
     @Override
     public void skipTurn() throws Exception {
-
+        server.skipTurn(nickname);
     }
 
     @Override
-    public void disconnect() throws Exception {
-
+    public void disconnect() {
+        // RMI doesn't request an explicit disconnection
     }
 }
