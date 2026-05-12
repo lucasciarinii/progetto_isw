7ac1db1893f0d0c10a165bc5f54fb03d04fc9690
EdoardoSacchi <edosacchi.04@gmail.com>
2026-05-12 14:03:27 +0200

    refactor(network): remove createServerAdapter method from NetworkAdapterFactory

--- src/main/java/org/example/network/NetworkAdapterFactory.java
+++ src/main/java/org/example/network/NetworkAdapterFactory.java
@@ -2,11 +2,8 @@ package org.example.network;
 
 import org.example.client.ClientController;
 import org.example.network.rmi.RMIClientNetworkAdapter;
-import org.example.network.rmi.RMIServerNetworkAdapter;
 import org.example.network.socket.SocketClientNetworkAdapter;
-import org.example.network.socket.SocketServerNetworkAdapter;
 
-import java.rmi.RemoteException;
 
 public class NetworkAdapterFactory {
 
@@ -18,14 +15,5 @@ public class NetworkAdapterFactory {
             case RMI -> new RMIClientNetworkAdapter(controller);
             case SOCKET -> new SocketClientNetworkAdapter(controller);
         };
-    }
-
-    // Server adapter
-    public static ServerNetworkAdapter createServerAdapter() {
-        try {
-            return new HybridServerNetworkAdapter();
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to create hybrid server: " + e.getMessage(), e);
-        }
     }
 }
